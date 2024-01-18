4830c298b16ec094b4635bea9edab354ba21cf81
lud-hu <ludwig.hubert@gmail.com>
2024-01-18 11:01:16 +0100

    minimal example

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,27 +1,15 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
-import { mergeConfig } from "vite";
-import userConfig from "./vite.config";
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/vue3-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
-  async viteFinal(baseConfig) {
-    return mergeConfig(baseConfig, userConfig);
-  },
+    stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+    framework: {
+        name: "@storybook/vue3-vite",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
 };
 
 export default config;
